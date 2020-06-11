8cb7d7d8ae3a7afd5433a484c800c802972f6300
choongsoo <4Lj9*6.c;Jf]oxmf2@2qKR]2>
2020-06-11 10:09:30 -0400

    Fixed the long description.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name="chobo",
-        version="0.99.0",
+        version="0.99.1",
         packages=setuptools.find_packages(),
         description="A Python package for graphical displays",
-        long_description="The chobo module is a forked version of the intrographics module developed by Dr. Lisa Torrey.\nIt allows you to create graphical displays in your Python 3 programs. The name chobo (초보) is a Korean word that means a beginner/novice, and the name is to inspire beginners at programming to give it a try.\nIt is written in Python 3, and uses the pygame module as the back-end",
+        long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/choongsoo/chobo",
         author="Choong-Soo Lee",
